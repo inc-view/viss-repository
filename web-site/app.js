507264e79ae71593abbff16efa5c08f6ef79f90b
leandroTokudome <leandro.tokudome@sptech.school>
2023-10-07 12:37:58 -0300

    PR da branch 'dashboardLeandro'

--- web-site/app.js
+++ web-site/app.js
@@ -12,7 +12,12 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
+var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
+
+// Meus Route's
+
+var leandroRouter = require("./src/routes/routeLeandro")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,7 +29,12 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
+app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
+
+// Meus Route's
+
+app.use("/routeLeandro", leandroRouter)
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
