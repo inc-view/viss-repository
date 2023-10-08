a3575e58e1f42a571cdbcc8fa592bbd3a4cfc069
eduufreire <eduardo.freire@sptech.school>
2023-10-08 17:42:39 -0300

    Sistema de cadastro em lote finalizado - v1

--- web-site/src/controllers/fileController.js
+++ web-site/src/controllers/fileController.js
@@ -1,1 +1,41 @@
+const csv = require("csvtojson");
+const fs = require("fs");
 
+var fileModel = require("../models/fileModel");
+
+function salvarDados(req, res, fileName) {
+
+  var filePath = `${__dirname}../../../uploads/${fileName}`;
+  csv()
+    .fromFile(filePath)
+    .then((data) => {
+      var valuesInsert = [];
+      for (row in data) {
+        valuesInsert.push(
+          `(null, NULL, 1, '${data[row].nome}', '${data[row].email}', '${data[row].cpf}', '${data[row].telefone}', '${data[row].senha}')`
+        );
+      }
+
+      fileModel
+        .insertBatch(valuesInsert)
+        .then((result) => {
+
+          if (result) {
+            res.redirect('http://localhost:3333/teste.html')
+            res.status(200).end();
+          }
+
+        })
+        .catch((error) => {
+          res.status(400).send(`${error}`);
+        });
+    })
+    .catch((error) => {
+      res.status(400).send(`Error -> fileController ${error}`);
+    });
+}
+
+
+module.exports = {
+  salvarDados,
+};
--- web-site/src/routes/files.js
+++ web-site/src/routes/files.js
@@ -1,35 +1,39 @@
-const formidable = require('formidable');
 var express = require("express");
 var router = express.Router();
 
-var fileController = require("../controllers/fileController.js");
+const path = require('path')
+const fs = require('fs');
+const formidable = require('formidable');
+
+var fileController = require("../controllers/fileController");
 
 
-router.post("/arquivo", function(req, res, next){
-
-  const path = require('path')
-  const fs = require('fs');
-  const formidable = require('formidable');
-
+router.post("/arquivo", function(req, res){
 
   const options = {
-    filter: function ({name, originalFilename, mimetype}) {
-      return mimetype && mimetype.includes("csv");
-    }
+    uploadDir: `${__dirname}/../../uploads`
   };
-
   const form = new formidable.IncomingForm(options);
 
+  var newFilename = ''
   form.parse(req, (err, fields, files) => {
-    var newName = files.fileCsv[0].newFilename
-    var oldpath = files.fileCsv[0].filepath
-    var newpath = path.join(__dirname, '../../uploads', `${newName}.csv`)
-    fs.renameSync(oldpath, newpath);
-    res.send('File uploaded and moved!');
+
+    if(files.fileCsv[0].mimetype.endsWith('csv')){
+
+      if(err){
+        res.status(400).send('Erro no processo de ler o arquivo')
+        return;
+      }
+
+      newFilename = files.fileCsv[0].newFilename
+      fileController.salvarDados(req, res, newFilename)
+      
+    }else{
+      res.status(400).send("Arquivo inválido")
+    }
+
   });
-
-
+  
 })
-
 
 module.exports = router;
