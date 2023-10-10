1ceaa987f31ff66e44c59fff4864f0271ab4cf53
brenodelmondes <breno.delmondes@sptech.school>
2023-10-10 15:29:21 -0300

    Conexão com o banco feita

--- python-api/api_dados.py
+++ python-api/api_dados.py
@@ -69,7 +69,6 @@ connection = mysql.connector.connect(
 
 cursor = connection.cursor()
 
-
 def showText():
     print(f"""{consoleColors['cyan']}
 []=================================================================[]
@@ -98,10 +97,11 @@ opcao = ""
 
 while not opcao in ("1", "2", "3"):
     print("Escolha uma opção:\n1- Registrar dados\n2- Exibir histórico\n3- Sair\n")
+    """ print(psutil.net_if_addrs()) """
     opcao = input()
 
 if opcao == "3":
-    print("")
+    print("Processos finalizados")
 
 """ elif opcao == "2":
     fig = plt.figure()
@@ -152,37 +152,120 @@ if opcao == "1":
 
         cpusPercent = psutil.cpu_percent(interval=1, percpu=True)
         memory = (psutil.virtual_memory())
-        memPercent = memory.percent
+        memoryPercent = memory.percent
         memoryUsed = round((memory.used / 1024 / 1024 / 1000), 1)
         memoryTotal = round((memory.total / 1024 / 1024 / 1000), 1)
         diskPartitions = psutil.disk_partitions()
         diskPercent = psutil.disk_usage(diskPartitions[0].mountpoint)
         dateNow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+        diskUseList = []
+
+        # A variável diskBar é um array de strings, onde vão sendo armazenadas as barras
+        # que são exibidas no console. Assim como a diskUseList, cada posição deste array
+        # armazena a barra referente à uma partição.
+        diskBar = []
+        # percorremos todas as partições obtidas a partir da psutil. A função enumerate  é do
+        # próprio python, e basicamente ela nos "dá" um id para cada item.
+        # Seria possível usar o for com uma sintaxe do tipo
+        #   for part in partitions:
+        # Porém,  tem o problema que neste caso não temos como saber em qual índice estamos
+        # atualmente, sem computações adicionais. A função enumerate simplifica esse processo,
+        # nos dando tanto o item do array quanto seu respectivo índice (representados por part
+        # e k neste for, respectivamente)
+        for (k, part) in enumerate(diskPartitions):
+            # Usamos a propriedade "device" da partição, para obter o seu uso de disco, e adicionamos
+            # no array diskUseList
+            diskUseList.append(psutil.disk_usage(part.device))
+            # Também aproveitamenos para já começar a criação da barra de porcentagem que é exibida
+            # no console.
+            if (diskUseList[k].percent <= 70):
+                diskBar.append(consoleColors['green'] + "|")
+            elif (diskUseList[k].percent <= 90):
+                diskBar.append(consoleColors['yellow'] + "|")
+            else:
+                diskBar.append(consoleColors['red'] + "|")
+                # A função virtual_memory retorna todas as estatístcas de uso da RAM, como
+        # Total disponível, quanto esta sendo usado, porcentagem usada, etc.
+        cpuBar = ""
+        ramBar = ""
+            # Assim como no disco acima, criamos o início das barras que serão exibidas no console,
+            # com as respectivas métricas.
+        if (cpusPercent[i] <= 70):
+            cpuBar = consoleColors['green'] + "|"
+        elif (cpusPercent[i] <= 90):
+            cpuBar = consoleColors['yellow'] + "|"
+        else:
+            cpuBar = consoleColors['red'] + "|"
+        if (memoryPercent <= 70):
+            ramBar = consoleColors['green'] + "|"
+        elif (memoryPercent <= 90):
+            ramBar = consoleColors['yellow'] + "|"
+        else:
+            ramBar = consoleColors['red'] + "|"
+           
+        # no caso do disco, novamente temos que usar um for para percorrer todas as partições.
+        for (k, disk) in enumerate(diskUseList):
+            if (diskPercent.percent <= i*2):
+                diskBar[k] += " "
+            else:
+                diskBar[k] += "="
+        if (memoryPercent <= i*2):
+            ramBar += " "
+        else:
+            ramBar += "=" 
+
+
+        # Finalizamos as barras, adicionando um "|" e a cor branca.
+        # É importante voltar a cor para branco aqui, se não essas cores começam
+        # a vazar para o resto do console.
+        cpuBar += "|" + consoleColors['white']          
+        ramBar += "|" + consoleColors['white']  
+
+        for (k,disk) in enumerate(diskUseList):        
+            diskBar[k] += "|" + consoleColors['white']
+
         os.system(systemClear)
 
         showText()
-        print("Memória Total: ", memoryTotal, "GB", "\nMemoria Usada: ", memoryUsed, "GB" "\nMemória Percent: ", memPercent, "%")
+        print("Memória Percent: ", memoryPercent, "%\n", ramBar, "\nMemória Total: ", memoryTotal, "GB", "\nMemoria Usada: ", memoryUsed, "GB")
         
         somaCpus = 0
         mediaCpus = 0
         for i in range(psutil.cpu_count()):
             somaCpus += cpusPercent[i]
             cpuName1 = (f"CPU{i+1}")
+            for j in range(51):
+                if (cpusPercent[i] <= j*2):
+                    cpuBar += " "
+                else:
+                    cpuBar += "="
             print(cpuName1, ":", cpusPercent[i], "%")
             
         mediaCpus = round((somaCpus / len(cpusPercent)),2)
 
         try:
-            mySqlInsertQueryCpuPercent = "INSERT INTO registro (registro, dtHora,fkComponenteComputador) VALUES (null, " + str(mediaCpus) + ",  now(), 1);"
+            mySqlInsertQueryCpuPercent = "INSERT INTO registro VALUES (null, " + str(mediaCpus) + ",  current_timestamp(), 1);"
+            mySqlInsertQueryMemoryPercent = "INSERT INTO registro VALUES (null, " + str(memoryPercent) + ",  current_timestamp(), 2);"
+            mySqlInsertQueryMemoryUsed = "INSERT INTO registro VALUES (null, " + str(memoryUsed) + ",  current_timestamp(), 3);"
+            mySqlInsertQueryMemoryTotal = "INSERT INTO registro VALUES (null, " + str(memoryTotal) + ",  current_timestamp(), 3);"
+            mySqlInsertQueryDiskPercent = "INSERT INTO registro VALUES (null, " + str(mediaCpus) + ",  current_timestamp(), 4);"
+             
             cursor.execute(mySqlInsertQueryCpuPercent)
+            cursor.execute(mySqlInsertQueryMemoryPercent)
+            cursor.execute(mySqlInsertQueryMemoryUsed)
+            cursor.execute(mySqlInsertQueryMemoryTotal)
+            cursor.execute(mySqlInsertQueryDiskPercent)
+            
             connection.commit()
-            cursor.close()
 
         except mysql.connector.Error as error:
            print("Failed to insert record into Laptop table {}".format(error))
 
         time.sleep(2)
     
-        if connection.is_connected():
-            connection.close()
+
+if connection.is_connected():
+    cursor.close()
+    connection.close()
+
