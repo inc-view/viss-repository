5812e626bbd02b80572821c1b359c10fe5c84ade
eduufreire <eduardo.freire@sptech.school>
2023-10-18 20:08:05 -0300

    Finalizando a funcionalidade de editar e excluir

--- web-site/src/models/funcionarioModel.js
+++ web-site/src/models/funcionarioModel.js
@@ -19,7 +19,26 @@ function listar(fkEmpresa){
 
 }
 
+
+function listarDadosFuncionario(id, fkEmpresa){
+
+  var querySql = `select * from funcionario
+                  where idFuncionario = ${id} and fkEmpresa = ${fkEmpresa};`
+  return database.executar(querySql)
+
+}
+
+
+function editarFuncionario(dados){
+  var querySql = `update funcionario
+                  set nome = '${dados.nome}', email = '${dados.email}', telefone = '${dados.telefone}', senha = '${dados.senha}'
+                  where idFuncionario = ${dados.id} and fkEmpresa = ${dados.empresa}`
+  return database.executar(querySql)
+}
+
 module.exports = {
   insert,
-  listar
+  listar,
+  listarDadosFuncionario,
+  editarFuncionario
 }
--- web-site/src/routes/funcionario.js
+++ web-site/src/routes/funcionario.js
@@ -11,4 +11,12 @@ router.get("/listar/:fkEmpresa", (req, res)=>{
   funcionarioController.listarFuncionario(req, res);
 });
 
+router.post("/listarFuncionario", (req, res)=>{
+  funcionarioController.listarFuncionarioEdit(req, res);
+})
+
+router.post("/editarFuncionario", (req, res)=>{
+  funcionarioController.editarFuncionario(req, res);
+})
+
 module.exports = router;
