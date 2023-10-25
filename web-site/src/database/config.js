e39c0563ace650b5f842a9a7f973bb9371ecaf13
LarissaSonoda <sonoda.lari@gmail.com>
2023-10-25 10:19:10 -0300

    arrumando erros

--- web-site/src/database/config.js
+++ web-site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "inkView",
     user: "root",
-    password: "@eduufreire",
+    password: "1234567",
 };
 
 function executar(instrucao) {
