c3e9123798f45a65bd1c59f9fb2935dff72cc02a
LarissaSonoda <sonoda.lari@gmail.com>
2023-11-24 14:39:24 -0300

    adicionando procedure para gráfico individual

--- web-site/src/models/LA-procIlicModel.js
+++ web-site/src/models/LA-procIlicModel.js
@@ -2,7 +2,7 @@ const database = require("../database/config")
 
 
 function buscarGraf1(fkEmpresa){
-
+  
   var querySql = `CALL spSelectGraf1(${fkEmpresa});`
   return database.executar(querySql)
 
