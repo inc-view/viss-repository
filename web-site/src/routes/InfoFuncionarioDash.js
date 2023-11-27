66c3104d622b8595b93a9af0d30b7b95ef512be6
Lucas1Ferreira <lucas.bezerra@sptech>
2023-11-27 14:52:40 -0300

    Upando selects SQL

--- web-site/src/routes/InfoFuncionarioDash.js
+++ web-site/src/routes/InfoFuncionarioDash.js
@@ -1,27 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
-var dashListagemController = require("../controllers/InfoFuncionarioDashController");
+var infoFuncionarioDashController = require("../controllers/InfoFuncionarioDashController");
 
-router.get("/ListagemCpuON", function (req, res) {
-    dashListagemController.ListagemCpuON(req, res);
+router.get("/ListagemDuracao", function (req, res) {
+    infoFuncionarioDashController.ListagemDuracao(req, res);
 });
-router.get("/ListagemCpuOff", function (req, res) {
-    dashListagemController.ListagemCpuOff(req, res);
+router.get("/ListagemTMA", function (req, res) {
+    infoFuncionarioDashController.ListagemTMA (req, res);
 });
 router.get("/fazerLista", function(req,res) {
-    dashListagemController.fazerLista(req,res);
+    infoFuncionarioDashController.fazerLista(req,res);
 });
+
+router.get("/ListagemTotalChamadas", function(req,res) {
+    infoFuncionarioDashController.ListagemTotalChamadas(req,res);
+});
+
 
 router.post("/fazerListaPorNome", function(req,res) {
-    dashListagemController.fazerListaPorNome(req,res);
+    infoFuncionarioDashController.fazerListaPorNome(req,res);
 });
 
-router.get("/ListagemTotalComputadores", function(req, res){
-        dashListagemController.ListagemTotalComputadores(req, res);
-});
-router.get("/ListagemCpusProblema", function(req, res){
-    dashListagemController.ListagemCpuProblema(req, res);
-});
 
 module.exports = router;
