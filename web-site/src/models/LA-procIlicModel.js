f9ad874ee68fa2de063095ba1d379c4f6cf6128d
LarissaSonoda <sonoda.lari@gmail.com>
2023-11-26 23:34:49 -0300

    alterações

--- web-site/src/models/LA-procIlicModel.js
+++ web-site/src/models/LA-procIlicModel.js
@@ -2,8 +2,11 @@ const database = require("../database/config")
 
 
 function buscarGraf1(fkEmpresa){
-  
-  var querySql = `CALL spSelectGraf1(${fkEmpresa});`
+  if(process.env.AMBIENTE_PROCESSO == 'producao'){
+
+  }else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+    var querySql = `CALL spSelectGraf1(${fkEmpresa});`
+  }
   return database.executar(querySql)
 
 }
