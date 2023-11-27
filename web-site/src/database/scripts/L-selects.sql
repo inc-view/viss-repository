d43e611efd7aa505369eefd09e597447c970061c
Lucas1Ferreira <lucas.bezerra@sptech>
2023-11-27 14:26:32 -0300

    dash funfando sem o modal

--- web-site/src/database/scripts/L-selects.sql
+++ web-site/src/database/scripts/L-selects.sql
@@ -16,7 +16,7 @@ JOIN funcionario f ON lf.fkFuncionario = f.idFuncionario
 JOIN empresa e ON f.fkEmpresa = e.idEmpresa
 WHERE e.idEmpresa = ${fkEmpresa};
 
--- KPI TODAS AS CHAMADAS 
+-- KPI TODAS AS CHAMADAS/AWS
 SELECT 
     sum(lf.abandonadas) as chamadasAbandonadas
 FROM ligacoesFuncionario lf
@@ -32,6 +32,15 @@ JOIN funcionario f ON lf.fkFuncionario = f.idFuncionario
 JOIN empresa e ON f.fkEmpresa = e.idEmpresa
 WHERE e.idEmpresa = ${fkEmpresa};
 
+-- KPI TEMPO MEDIO DURACAO CHAMADA AWS
+
+SELECT 
+    CONVERT(varchar, DATEADD(SECOND, AVG(DATEDIFF(SECOND, '00:00:00', lf.duracao)), '00:00:00'), 108) AS tempoMedioDuracao
+FROM ligacoesFuncionario lf
+JOIN funcionario f ON lf.fkFuncionario = f.idFuncionario
+JOIN empresa e ON f.fkEmpresa = e.idEmpresa
+WHERE e.idEmpresa = 1;
+
 -- TMA DA EMPRESA
 SELECT 
     avg((atendidas) / TIME_TO_SEC(duracao)) AS TMA
@@ -43,9 +52,25 @@ JOIN
     empresa e ON f.fkEmpresa = e.idEmpresa
 WHERE 
     e.idEmpresa = ${fkEmpresa};
+-- TMA da EMPRESA AWS
+SELECT
+    AVG(lf.atendidas / 
+        (DATEPART(HOUR, lf.duracao)*3600.0 + 
+         DATEPART(MINUTE, lf.duracao)*60.0 + 
+         DATEPART(SECOND, lf.duracao))) AS TMA
+FROM 
+    ligacoesFuncionario lf
+JOIN 
+    funcionario f ON lf.fkFuncionario = f.idFuncionario
+JOIN 
+    empresa e ON f.fkEmpresa = e.idEmpresa
+WHERE 
+    e.idEmpresa = ${fkEmpresa};
+
 
 
     -- lista com TMA
+
 
     SELECT
     f.nome AS nome_funcionario,
@@ -64,6 +89,33 @@ WHERE
 ORDER BY 
     lf.atendidas DESC;
 
+-- LISTA COM TMA AWS
 
+SELECT
+    f.nome AS nome_funcionario,
+    lf.recebidas AS chamadas_recebidas,
+    lf.atendidas AS chamadas_atendidas,
+    lf.porcAtendidas AS porcentagem_atendidas,
+    lf.abandonadas AS chamadas_abandonadas,
+    lf.duracao AS duracao_total,
+    AVG(lf.atendidas / 
+        (DATEPART(HOUR, CAST(lf.duracao AS DATETIME))*3600.0 + 
+         DATEPART(MINUTE, CAST(lf.duracao AS DATETIME))*60.0 + 
+         DATEPART(SECOND, CAST(lf.duracao AS DATETIME)))) AS TMA
+FROM 
+    funcionario f
+JOIN 
+    ligacoesFuncionario lf ON f.idFuncionario = lf.fkFuncionario
+WHERE 
+    f.fkEmpresa = ${fkEmpresa}
+GROUP BY
+    f.nome,
+    lf.recebidas,
+    lf.atendidas,
+    lf.porcAtendidas,
+    lf.abandonadas,
+    lf.duracao
+ORDER BY 
+    lf.atendidas DESC;
 
 
--- web-site/src/models/InfoFuncionarioDashModel.js
+++ web-site/src/models/InfoFuncionarioDashModel.js
@@ -4,7 +4,13 @@ function ListagemTotalChamadas(fkEmpresa){
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = ``;
+        instrucaoSql = `SELECT 
+    sum(lf.abandonadas) as chamadasAbandonadas
+FROM ligacoesFuncionario lf
+JOIN funcionario f ON lf.fkFuncionario = f.idFuncionario
+JOIN empresa e ON f.fkEmpresa = e.idEmpresa
+WHERE e.idEmpresa = ${fkEmpresa};
+`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `SELECT 
         sum(lf.abandonadas) as chamadasAbandonadas
@@ -24,7 +30,19 @@ function ListagemTMA(fkEmpresa){
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = ``;
+        instrucaoSql = `SELECT
+    AVG(lf.atendidas / 
+        (DATEPART(HOUR, lf.duracao)*3600.0 + 
+         DATEPART(MINUTE, lf.duracao)*60.0 + 
+         DATEPART(SECOND, lf.duracao))) AS TMA
+FROM 
+    ligacoesFuncionario lf
+JOIN 
+    funcionario f ON lf.fkFuncionario = f.idFuncionario
+JOIN 
+    empresa e ON f.fkEmpresa = e.idEmpresa
+WHERE 
+    e.idEmpresa = ${fkEmpresa};`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `SELECT 
         avg((atendidas) / TIME_TO_SEC(duracao)) AS TMA
@@ -49,7 +67,12 @@ function ListagemDuracao(fkEmpresa){
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = ``;
+        instrucaoSql = `SELECT 
+    CONVERT(varchar, DATEADD(SECOND, AVG(DATEDIFF(SECOND, '00:00:00', lf.duracao)), '00:00:00'), 108) AS tempoMedioDuracao
+FROM ligacoesFuncionario lf
+JOIN funcionario f ON lf.fkFuncionario = f.idFuncionario
+JOIN empresa e ON f.fkEmpresa = e.idEmpresa
+WHERE e.idEmpresa = ${fkEmpresa};`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `SELECT TIME_FORMAT(SEC_TO_TIME(AVG(TIME_TO_SEC(lf.duracao))), '%H:%i:%s') AS tempoMedioDuracao
         FROM ligacoesFuncionario lf
@@ -70,7 +93,33 @@ function fazerLista(fkEmpresa){
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = ``;
+        instrucaoSql = `
+        SELECT
+    f.nome AS nome_funcionario,
+    lf.recebidas AS chamadas_recebidas,
+    lf.atendidas AS chamadas_atendidas,
+    lf.porcAtendidas AS porcentagem_atendidas,
+    lf.abandonadas AS chamadas_abandonadas,
+    lf.duracao AS duracao_total,
+    AVG(lf.atendidas / 
+        (DATEPART(HOUR, CAST(lf.duracao AS DATETIME))*3600.0 + 
+         DATEPART(MINUTE, CAST(lf.duracao AS DATETIME))*60.0 + 
+         DATEPART(SECOND, CAST(lf.duracao AS DATETIME)))) AS TMA
+FROM 
+    funcionario f
+JOIN 
+    ligacoesFuncionario lf ON f.idFuncionario = lf.fkFuncionario
+WHERE 
+    f.fkEmpresa = 1
+GROUP BY
+    f.nome,
+    lf.recebidas,
+    lf.atendidas,
+    lf.porcAtendidas,
+    lf.abandonadas,
+    lf.duracao
+ORDER BY 
+    lf.atendidas DESC;`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `
         SELECT
@@ -106,7 +155,32 @@ function fazerListaPorNome(fkEmpresa,nome){
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = ``;
+        instrucaoSql = `SELECT
+    f.nome AS nome_funcionario,
+    lf.recebidas AS chamadas_recebidas,
+    lf.atendidas AS chamadas_atendidas,
+    lf.porcAtendidas AS porcentagem_atendidas,
+    lf.abandonadas AS chamadas_abandonadas,
+    lf.duracao AS duracao_total,
+    AVG(lf.atendidas / 
+        (DATEPART(HOUR, CAST(lf.duracao AS DATETIME))*3600.0 + 
+         DATEPART(MINUTE, CAST(lf.duracao AS DATETIME))*60.0 + 
+         DATEPART(SECOND, CAST(lf.duracao AS DATETIME)))) AS TMA
+FROM 
+    funcionario f
+JOIN 
+    ligacoesFuncionario lf ON f.idFuncionario = lf.fkFuncionario
+WHERE 
+    f.fkEmpresa = ${fkEmpresa} and f.nome like '%${nome}%'
+GROUP BY
+    f.nome,
+    lf.recebidas,
+    lf.atendidas,
+    lf.porcAtendidas,
+    lf.abandonadas,
+    lf.duracao
+ORDER BY 
+    lf.atendidas DESC;`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `
         SELECT
