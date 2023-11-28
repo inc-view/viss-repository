84d27447e6e1dbef93bd2690c2d83e9c9e6b8250
eduufreire <eduardo.freire@sptech.school>
2023-11-27 21:16:18 -0300

    teste

--- python-api/api_dados.py
+++ python-api/api_dados.py
@@ -114,7 +114,7 @@ def CheckLogin():
             print(idFuncionario)
             try:
                 cursor.execute(f" call checkComputerExists ('{str(ipMaquina)}', '{platform.node()}', 'EC2 - AWS', 1, '{platform.system()}') ")
-                cursorSERVER.execute("exec checkComputerExists %s, %s, %s, %d, %s, %d", (ipMaquina, platform.node(), 'EC2 - AWS', idFuncionario[0], platform.system()))
+                cursorSERVER.execute("exec checkComputerExists %s, %s, %s, %d, %s", (ipMaquina, platform.node(), 'EC2 - AWS', idFuncionario[0], platform.system()))
                 connection.commit()
                 connSERVER.commit()
                 
